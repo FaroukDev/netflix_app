50ca2a9104ff6c12ce6f43682d000c9a17cc88fe
FaroukDev <fa.elayache1@gmail.com>
2020-10-28 10:08:09 +0100

    add path of image on Posterlist

--- src/components/PosterList.js
+++ src/components/PosterList.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+
 import Poster from './Poster';
 import '../css/PosterList.css';
+
+const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p';
+const POSTER_SIZE = 'w500';
 let wish;
 
 class PosterList extends Component {
     renderPoster = () => {
         return this.props.movies.map(movie => {
-            const imgSrc = movie.poster_path;
+            const imgSrc = `${IMAGE_BASE_URL}/${POSTER_SIZE}/${movie.poster_path}` 
             wish = false;
             return (
                 <Poster 
