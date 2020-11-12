7b38ce2d218a232560013a21479ca8767138646a
FaroukDev <fa.elayache1@gmail.com>
2020-11-12 10:22:22 +0100

    add MvPlayerList

---
+++ src/components/MvPlayerListItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class MvPlayerListItem extends Component {
+  render() {
+      const activeClass = this.props.active ? 'mvPlayerListItem active' : 'mvPlayerListItem'
+    return (
+      <>
+        <div className={activeClass}>
+          <div className="mvPlayerListItem--number">{this.props.number}</div>
+          <div className="mvPlayerListItem--title">{this.props.title}</div>
+          <div className="mvPlayerListItem--time">{this.props.duration}</div>
+        </div>
+        <div className="mvPlayerListItem--divider" />
+      </>
+    );
+  }
+}
+
+export default MvPlayerListItem;
--- src/routes/MoviePlayer.js
+++ src/routes/MoviePlayer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ReactPlayer from './react-player';
+import VideoPlayer from '../components/VideoPlayer';
+import MvPlayerList from '../components/VideoPlayer';
 
 export default class MoviePlayer extends Component {
     render() {
